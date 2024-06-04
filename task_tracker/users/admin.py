4de9e3f1af58db9b49592d6eee1e29d6391629ad
Виктор Тузовский <aletissarichard@gmail.com>
2024-06-04 20:19:20 +0700

    Added docstrings

--- task_tracker/users/admin.py
+++ task_tracker/users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+class BaseAdmin(admin.ModelAdmin):
+	"""Базовая модель админки."""
+	empty_value_display = '-пусто-'
+	list_per_page = 40
+
+
+@admin.register(User)
+class UserAdmin(BaseAdmin):
+	"""Модель админки для Пользователя."""
+	fields_to_display_filter_search = (
+		'last_name',
+		'first_name',
+		'username',
+		'email',
+		'phone'
+	)
+	list_display = fields_to_display_filter_search
+	list_filter = fields_to_display_filter_search
+	search_fields = fields_to_display_filter_search
